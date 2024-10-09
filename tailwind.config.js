9d4335d689f2e6c242ca428a6918af5499fed384
Thắng Trần Minh <84889893+tranminhthang1606@users.noreply.github.com>
2024-10-09 15:44:24 +0700

    base temp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        '2xl': '1920px',
+      },
+    },
+    
     extend: {},
   },
   plugins: [],
